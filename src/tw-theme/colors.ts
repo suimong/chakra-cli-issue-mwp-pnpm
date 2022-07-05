c47cb9773ac42d025dac056b063361101266ceb6
Ben <bennyyjx@gmail.com>
2022-07-05 20:51:19 +0800

    adding 'baseUrl' to tsconfig.json: chakra-cli fail

--- src/tw-theme/colors.ts
+++ src/tw-theme/colors.ts
@@ -1,4 +1,4 @@
-import color from "color";
+// import color from "color";
 
 const config = {
   darkest: {
@@ -13,69 +13,69 @@ const config = {
   },
 };
 
-function createShades(centerColor: string): Record<number, string> {
-  const _color = centerColor;
-  const darkSteps = 4;
-  const lightSteps = 5;
+// function createShades(centerColor: string): Record<number, string> {
+//   const _color = centerColor;
+//   const darkSteps = 4;
+//   const lightSteps = 5;
 
-  const lightnessStep = (config.lightest.lightness - 50) / lightSteps;
-  const darknessStep = (50 - config.darkest.lightness) / darkSteps;
+//   const lightnessStep = (config.lightest.lightness - 50) / lightSteps;
+//   const darknessStep = (50 - config.darkest.lightness) / darkSteps;
 
-  const lightRotateStep = config.lightest.rotate / lightSteps;
-  const darkRotateStep = config.darkest.rotate / darkSteps;
+//   const lightRotateStep = config.lightest.rotate / lightSteps;
+//   const darkRotateStep = config.darkest.rotate / darkSteps;
 
-  const lightSaturateStep = config.lightest.saturate / lightSteps;
-  const darkSaturateStep = config.darkest.saturate / darkSteps;
+//   const lightSaturateStep = config.lightest.saturate / lightSteps;
+//   const darkSaturateStep = config.darkest.saturate / darkSteps;
 
-  return {
-    50: color(_color)
-      .lightness(50 + lightnessStep * 5)
-      .rotate(lightRotateStep * 5)
-      .saturate(lightSaturateStep * 5)
-      .hex(),
-    100: color(_color)
-      .lightness(50 + lightnessStep * 4)
-      .rotate(lightRotateStep * 4)
-      .saturate(lightSaturateStep * 4)
-      .hex(),
-    200: color(_color)
-      .lightness(50 + lightnessStep * 3)
-      .rotate(lightRotateStep * 3)
-      .saturate(lightSaturateStep * 3)
-      .hex(),
-    300: color(_color)
-      .lightness(50 + lightnessStep * 2)
-      .rotate(lightRotateStep * 2)
-      .saturate(lightSaturateStep * 2)
-      .hex(),
-    400: color(_color)
-      .lightness(50 + Number(lightnessStep))
-      .rotate(Number(lightRotateStep))
-      .saturate(Number(lightSaturateStep))
-      .hex(),
-    500: centerColor,
-    600: color(_color)
-      .lightness(50 - Number(darknessStep))
-      .rotate(Number(darkRotateStep))
-      .saturate(Number(darkSaturateStep))
-      .hex(),
-    700: color(_color)
-      .lightness(50 - darknessStep * 2)
-      .rotate(darkRotateStep * 2)
-      .saturate(darkSaturateStep * 2)
-      .hex(),
-    800: color(_color)
-      .lightness(50 - darknessStep * 3)
-      .rotate(darkRotateStep * 3)
-      .saturate(darkSaturateStep * 3)
-      .hex(),
-    900: color(_color)
-      .lightness(50 - darknessStep * 4)
-      .rotate(darkRotateStep * 4)
-      .saturate(darkSaturateStep * 4)
-      .hex(),
-  };
-}
+//   return {
+//     50: color(_color)
+//       .lightness(50 + lightnessStep * 5)
+//       .rotate(lightRotateStep * 5)
+//       .saturate(lightSaturateStep * 5)
+//       .hex(),
+//     100: color(_color)
+//       .lightness(50 + lightnessStep * 4)
+//       .rotate(lightRotateStep * 4)
+//       .saturate(lightSaturateStep * 4)
+//       .hex(),
+//     200: color(_color)
+//       .lightness(50 + lightnessStep * 3)
+//       .rotate(lightRotateStep * 3)
+//       .saturate(lightSaturateStep * 3)
+//       .hex(),
+//     300: color(_color)
+//       .lightness(50 + lightnessStep * 2)
+//       .rotate(lightRotateStep * 2)
+//       .saturate(lightSaturateStep * 2)
+//       .hex(),
+//     400: color(_color)
+//       .lightness(50 + Number(lightnessStep))
+//       .rotate(Number(lightRotateStep))
+//       .saturate(Number(lightSaturateStep))
+//       .hex(),
+//     500: centerColor,
+//     600: color(_color)
+//       .lightness(50 - Number(darknessStep))
+//       .rotate(Number(darkRotateStep))
+//       .saturate(Number(darkSaturateStep))
+//       .hex(),
+//     700: color(_color)
+//       .lightness(50 - darknessStep * 2)
+//       .rotate(darkRotateStep * 2)
+//       .saturate(darkSaturateStep * 2)
+//       .hex(),
+//     800: color(_color)
+//       .lightness(50 - darknessStep * 3)
+//       .rotate(darkRotateStep * 3)
+//       .saturate(darkSaturateStep * 3)
+//       .hex(),
+//     900: color(_color)
+//       .lightness(50 - darknessStep * 4)
+//       .rotate(darkRotateStep * 4)
+//       .saturate(darkSaturateStep * 4)
+//       .hex(),
+//   };
+// }
 
 const themeColors = {
   transparent: "transparent",
@@ -227,8 +227,8 @@ const themeColors = {
 
 export const colors = {
   // generated
-  discord: createShades("#314db5"),
-  opensea: createShades("#3191e8"),
+  // discord: createShades("#314db5"),
+  // opensea: createShades("#3191e8"),
 
   // theme colors
   ...themeColors,
